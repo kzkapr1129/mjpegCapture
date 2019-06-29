735ce63c5d73dd66e4b0c9e5f5d61fb18a813914
中山一輝 <nakayama@MacBook.local>
2019-06-29 23:42:24 +0900

    引数から接続先アドレスを指定できるようにした

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s ip_addr\n", argv[0]);
+		return 0;
+	}
+
 	MJpegCapture cap;
-	cap.start("192.168.11.6", 8080, "/?action=stream");
+	if (cap.start(argv[1], 8080, "/?action=stream")) {
+		printf("failed to connect: %s\n", argv[1]);
+		return -1;
+	}
 
 	while (true) {
 		cv::Mat frame;
